7020d454a4d3238ed222ba54b75629f6c76b89e5
Timothy Hufler <support@highhandpro.com>
2026-08-13 06:44:57 -0700

    Increment version label to v1.3

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -269,7 +269,7 @@ export const Sidebar: React.FC<SidebarProps> = ({ activeTab, setActiveTab, onSwi
             Penny Ante Poker Club
           </p>
           <p style={{ fontSize: '0.7rem', color: 'var(--text-muted)', textAlign: 'center', marginTop: '2px' }}>
-            v1.2 (Local-First + Sim)
+            v1.3 (Local-First + Sim)
           </p>
         </div>
       </div>
